21a80b7002d6a2c72ccb0c15957a11ced30fc03e
Nathakorn Chaninthanadecha <nathakorn@nsquared.asia>
2018-07-09 18:47:39 +0700

    update spec file

--- lib/stock_shaker/version.rb
+++ lib/stock_shaker/version.rb
@@ -2,5 +2,5 @@
 
 # Define rubygems version
 module StockShaker
-  VERSION = '0.1.3'.freeze
+  VERSION = '0.1.4'.freeze
 end
--- spec/lazada_open_platform/request_spec.rb
+++ spec/lazada_open_platform/request_spec.rb
@@ -1,7 +1,7 @@
 require 'stock_shaker/request'
 
 # TODO: Write the test!!!
-RSpec.describe StockShaker::Request do
+RSpec.describe StockShaker::LazadaOpenPlatform::Request do
   it 'does something useful' do
     expect(true).to eq(true)
   end
